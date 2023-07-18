00df84ee03bb2a357ee610edf6c49736bd28aef6
sspillane <sspillane@maystreet.com>
2023-07-18 13:41:06 -0400

    Ensure that openvpn uses saved credentials

--- breakbulk/fish/.config/fish/functions/vpn--start.fish
+++ breakbulk/fish/.config/fish/functions/vpn--start.fish
@@ -1,7 +1,14 @@
 function vpn--start
-sudo openvpn --config ~/.local/share/openvpn/maystreet.ovpn --daemon
-while not sudo resolvectl dns tun0 10.98.16.41 10.98.16.42 2> /dev/null
-echo -n .
-sleep 1
-end
+  echo "Is OpenVPN currently running?"
+  pgrep -fa openvpn
+
+  echo "Starting OpenVPN in daemon mode"
+  sudo openvpn --config ~/.local/share/openvpn/maystreet.ovpn --daemon
+  pgrep -fa openvpn
+
+  echo "Setting up DNS resolution"
+  while not sudo resolvectl dns tun0 10.98.16.41 10.98.16.42 2> /dev/null
+    echo -n .
+    sleep 10
+  end
 end
