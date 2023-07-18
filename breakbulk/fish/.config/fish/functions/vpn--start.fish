7bb37e18bb3890b02676147bc3a9322e65cb7b2b
sspillane <sspillane@maystreet.com>
2023-07-18 11:13:19 -0400

    Fix openvpn DNS setup issue

--- breakbulk/fish/.config/fish/functions/vpn--start.fish
+++ breakbulk/fish/.config/fish/functions/vpn--start.fish
@@ -1,3 +1,7 @@
 function vpn--start
-  sudo openvpn --config ~/.local/share/openvpn/maystreet.ovpn --daemon
+sudo openvpn --config ~/.local/share/openvpn/maystreet.ovpn --daemon
+while not sudo resolvectl dns tun0 10.98.16.41 10.98.16.42 2> /dev/null
+echo -n .
+sleep 1
+end
 end
