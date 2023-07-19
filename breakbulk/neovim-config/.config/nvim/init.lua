575c7fc0978243dea9fda306eee55071d4382ef0
sspillane <sspillane@maystreet.com>
2023-07-19 17:25:38 +0000

    Comment out nvim config that's broken
    For now, nvim under my fancy config is broken. I'm going to need some time
    to work on it, and that time isn't now. Therefore, I'm commenting-out the
    offending parts. In the end, I may just use submodules and capture stuff,
    rather than a flaky plugin manager. That's going to need time also.

--- breakbulk/neovim-config/.config/nvim/init.lua
+++ breakbulk/neovim-config/.config/nvim/init.lua
@@ -32,21 +32,21 @@ vim.g.splitright = 1
 
 -- Configure Plug and install plugins.
 -- Don't forget to run :CocInstall to complete COC setup.
-local Plug = vim.fn['plug#']
-vim.call('plug#begin')
-Plug 'folke/which-key.nvim'
-Plug ('neoclide/coc.nvim', {branch= 'release'})
-Plug 'tpope/vim-surround'
-Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-treesitter/nvim-treesitter'
-Plug 'nvim-telescope/telescope.nvim'
-Plug 'tpope/vim-fugitive'
-Plug 'shumphrey/fugitive-gitlab.vim'
-Plug 'easymotion/vim-easymotion'
-vim.call('plug#end')
+--local Plug = vim.fn['plug#']
+--vim.call('plug#begin')
+--Plug 'folke/which-key.nvim'
+--Plug ('neoclide/coc.nvim', {branch= 'release'})
+--Plug 'tpope/vim-surround'
+--Plug 'nvim-lua/plenary.nvim'
+--Plug 'nvim-treesitter/nvim-treesitter'
+--Plug 'nvim-telescope/telescope.nvim'
+--Plug 'tpope/vim-fugitive'
+--Plug 'shumphrey/fugitive-gitlab.vim'
+--Plug 'easymotion/vim-easymotion'
+--vim.call('plug#end')
 
 -- Fugitive: Configure gitlab plugin
-vim.g.fugitive_gitlab_domains = {'https://gitlab.svc.maystreet.com/'}
+--vim.g.fugitive_gitlab_domains = {'https://gitlab.svc.maystreet.com/'}
 
 -- Swap ; and :
 vim.cmd [[
@@ -82,76 +82,76 @@ vim.cmd [[
 ]]
 
 -- Set up COC
-vim.cmd [[
-  function! s:check_back_space() abort
-    let col = col('.') - 1
-    return !col || getline('.')[col - 1]  =~# '\s'
-  endfunction
-
-
-  " Use tab for trigger completion with characters ahead and navigate.
-  " NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
-  " other plugin before putting this into your config.
-  inoremap <silent><expr> <TAB>
-        \ pumvisible() ? "\<C-n>" :
-        \ <SID>check_back_space() ? "\<TAB>" :
-        \ coc#refresh()
-  inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
-  
-  
-  " Use <c-space> to trigger completion.
-  if has('nvim')
-    inoremap <silent><expr> <c-space> coc#refresh()
-  else
-    inoremap <silent><expr> <c-@> coc#refresh()
-  endif
-
-
-  " Make <CR> auto-select the first completion item and notify coc.nvim to
-  " format on enter, <cr> could be remapped by other vim plugin
-  inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
-                                \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
-
-
-  " Use K to show documentation in preview window.
-  nnoremap <silent> K :call <SID>show_documentation()<CR>
-
-  
-  function! s:show_documentation()
-    if (index(['vim','help'], &filetype) >= 0)
-      execute 'h '.expand('<cword>')
-    elseif (coc#rpc#ready())
-      call CocActionAsync('doHover')
-    else
-      execute '!' . &keywordprg . " " . expand('<cword>')
-    endif
-  endfunction
-
-  
-  " Highlight the symbol and its references when holding the cursor.
-  autocmd CursorHold * silent call CocActionAsync('highlight')
-
-  
-  " Add (Neo)Vim's native statusline support.
-  " NOTE: Please see `:h coc-status` for integrations with external plugins that
-  " provide custom statusline: lightline.vim, vim-airline.
-  " set statusline^=%{coc#status()}%{get(b:,'coc_current_function','')}
-
-  " Make the popup background not suck.
-  :hi Pmenu ctermbg=DarkGrey
-  :hi FgCocErrorFloatBgCocFloating ctermbg=Red ctermfg=White
-  :hi FgCocWarningFloatBgCocFloating ctermbg=DarkGrey ctermfg=Yellow
-
-  " Map [g ]g to jump around diagnostics
-  nmap <silent> [g <Plug>(coc-diagnostic-prev)
-  nmap <silent> ]g <Plug>(coc-diagnostic-next)
-]]
-
--- Set up easymotion search
-vim.cmd [[
-  map  / <Plug>(easymotion-sn)
-  omap / <Plug>(easymotion-tn)
-]]
+--vim.cmd [[
+--  function! s:check_back_space() abort
+--    let col = col('.') - 1
+--    return !col || getline('.')[col - 1]  =~# '\s'
+--  endfunction
+--
+--
+--  " Use tab for trigger completion with characters ahead and navigate.
+--  " NOTE: Use command ':verbose imap <tab>' to make sure tab is not mapped by
+--  " other plugin before putting this into your config.
+--  inoremap <silent><expr> <TAB>
+--        \ pumvisible() ? "\<C-n>" :
+--        \ <SID>check_back_space() ? "\<TAB>" :
+--        \ coc#refresh()
+--  inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+--  
+--  
+--  " Use <c-space> to trigger completion.
+--  if has('nvim')
+--    inoremap <silent><expr> <c-space> coc#refresh()
+--  else
+--    inoremap <silent><expr> <c-@> coc#refresh()
+--  endif
+--
+--
+--  " Make <CR> auto-select the first completion item and notify coc.nvim to
+--  " format on enter, <cr> could be remapped by other vim plugin
+--  inoremap <silent><expr> <cr> pumvisible() ? coc#_select_confirm()
+--                                \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+--
+--
+--  " Use K to show documentation in preview window.
+--  nnoremap <silent> K :call <SID>show_documentation()<CR>
+--
+--  
+--  function! s:show_documentation()
+--    if (index(['vim','help'], &filetype) >= 0)
+--      execute 'h '.expand('<cword>')
+--    elseif (coc#rpc#ready())
+--      call CocActionAsync('doHover')
+--    else
+--      execute '!' . &keywordprg . " " . expand('<cword>')
+--    endif
+--  endfunction
+--
+--  
+--  " Highlight the symbol and its references when holding the cursor.
+--  autocmd CursorHold * silent call CocActionAsync('highlight')
+--
+--  
+--  " Add (Neo)Vim's native statusline support.
+--  " NOTE: Please see `:h coc-status` for integrations with external plugins that
+--  " provide custom statusline: lightline.vim, vim-airline.
+--  " set statusline^=%{coc#status()}%{get(b:,'coc_current_function','')}
+--
+--  " Make the popup background not suck.
+--  :hi Pmenu ctermbg=DarkGrey
+--  :hi FgCocErrorFloatBgCocFloating ctermbg=Red ctermfg=White
+--  :hi FgCocWarningFloatBgCocFloating ctermbg=DarkGrey ctermfg=Yellow
+--
+--  " Map [g ]g to jump around diagnostics
+--  nmap <silent> [g <Plug>(coc-diagnostic-prev)
+--  nmap <silent> ]g <Plug>(coc-diagnostic-next)
+--]]
+--
+---- Set up easymotion search
+--vim.cmd [[
+--  map  / <Plug>(easymotion-sn)
+--  omap / <Plug>(easymotion-tn)
+--]]
 
 -- Set up leader menus.
 vim.g.mapleader = ' '
